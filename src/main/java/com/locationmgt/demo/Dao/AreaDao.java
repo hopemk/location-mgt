9f49ab9fbefab78f9293a9fe7c27024e7cef1a82
Hope Kugotsi <hopekugotsi@gmail.com>
2022-04-28 10:57:26 +0200

    shop service impl

--- src/main/java/com/locationmgt/demo/Dao/AreaDao.java
+++ src/main/java/com/locationmgt/demo/Dao/AreaDao.java
@@ -3,15 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.locationmgt.demo;
+package com.locationmgt.demo.Dao;
 
 import com.locationmgt.demo.entity.Shop;
+import java.awt.geom.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author nashey
  */
-public interface Shopdao extends JpaRepository<Shop, Long>{
+public interface AreaDao extends JpaRepository<Area, Long>{
     
 }
